6586d3987ee0b2fabce040112668cb7cf30305a8
belaeat <omyhossain@gmail.com>
2025-04-11 07:30:04 +0300

    fix: item storage

--- Frontend/src/pages/Home/Home.tsx
+++ Frontend/src/pages/Home/Home.tsx
@@ -1,10 +1,10 @@
 
 import Items from "../../components/Items/Items";
 
-
 const Home = () => {
     return (
         <div>
+            {/* Storage Items will be here */}
             <Items />
         </div>
     )
